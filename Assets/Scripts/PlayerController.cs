2fc8bc77407625e63bee1798c701387e0ebcd100
Satsuki884 <stasyaklim884@gmail.com>
2026-03-21 13:59:39 +0200

    added idle anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,29 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using Unity.Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float _currentSpeed;
     [SerializeField] private float _walkSpeed = 5f;
     [SerializeField] private float _sprintSpeed = 10f;
-    private CharacterController _characterController;
+
+    private float _currentSpeed;
+
+    private Rigidbody2D _rb;
+
     private Vector2 _moveInput;
-    private Vector3 _velocity;
 
     private void Awake()
     {
-        _characterController = GetComponent<CharacterController>();
+        if (_rb == null)
+            _rb = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
     {
         _currentSpeed = _walkSpeed;
     }
+
+    // INPUT SYSTEM
     public void OnMove(InputValue value)
     {
         _moveInput = value.Get<Vector2>();
@@ -28,21 +31,97 @@ public class PlayerController : MonoBehaviour
 
     public void OnSprint(InputValue value)
     {
-        if (value.Get<float>() > 0.5f)
+        _currentSpeed = value.Get<float>() > 0.5f ? _sprintSpeed : _walkSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        if (!Mouse.current.leftButton.isPressed)
         {
-            _currentSpeed = _sprintSpeed;
-        }
-        else
-        {
-            _currentSpeed = _walkSpeed;
+            RotateToMovement();
         }
     }
 
     private void Update()
     {
-        Vector2 move = new Vector2(_moveInput.x, _moveInput.y);
-        _characterController.Move(move * _currentSpeed * Time.deltaTime);
-
+        RotateToMouse();
     }
 
+    // РУХ
+    private void Move()
+    {
+        Vector2 move = _moveInput * _currentSpeed;
+        _rb.MovePosition(_rb.position + move * Time.fixedDeltaTime);
+    }
+
+    // ПОВОРОТ У НАПРЯМКУ РУХУ
+    private void RotateToMovement()
+    {
+        if (_moveInput.magnitude > 0.1f)
+        {
+            float angle = Mathf.Atan2(_moveInput.y, _moveInput.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10f * Time.fixedDeltaTime);
+        }
+    }
+
+    // ортографічний поворот до миші
+    // private void RotateToMouse()
+    // {
+    //     if (Mouse.current.leftButton.isPressed)
+    //     {
+    //         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+    //         mousePos.z = 0f;
+
+    //         Vector3 direction = mousePos - transform.position;
+
+    //         if (direction.sqrMagnitude > 0.01f)
+    //         {
+    //             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+    //             Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
+
+    //             transform.rotation = Quaternion.Lerp(
+    //                 transform.rotation,
+    //                 targetRotation,
+    //                 15f * Time.deltaTime
+    //             );
+    //         }
+    //     }
+    // }
+
+    // перспективний поворот до миші
+    private void RotateToMouse()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            // Площина на рівні персонажа (Z = позиція гравця)
+            Plane plane = new Plane(Vector3.forward, transform.position);
+
+            if (plane.Raycast(ray, out float distance))
+            {
+                Vector3 mouseWorldPos = ray.GetPoint(distance);
+
+                Vector3 direction = mouseWorldPos - transform.position;
+                direction.z = 0f;
+
+                if (direction.sqrMagnitude > 0.01f)
+                {
+                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                    Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
+
+                    transform.rotation = Quaternion.Lerp(
+                        transform.rotation,
+                        targetRotation,
+                        15f * Time.deltaTime
+                    );
+                }
+            }
+        }
+    }
 }
