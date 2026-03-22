9f2a99ee5e2fadcceeb219719af8c48b0e494a41
Satsuki884 <stasyaklim884@gmail.com>
2026-03-22 21:18:14 +0200

    fixed audio manager

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -8,27 +8,19 @@ public class MenuController : MonoBehaviour
     [SerializeField] private Button _quitButton;
     [SerializeField] private Button _infoButton;
     [SerializeField] private Button _closeInfoButton;
-    [SerializeField] private Button _soundButton;
 
-    [Header("Sound")]
-    [SerializeField] private Image _soundIcon;
-    [SerializeField] private Sprite _soundOnSprite;
-    [SerializeField] private Sprite _soundOffSprite;
     private bool isSoundEnabled;
     void Start()
     {
         Time.timeScale = 0f;
 
         InitializeButtons();
-        SoundStart();
     }
 
     void InitializeButtons()
     {
         _startButton.onClick.RemoveAllListeners();
         _startButton.onClick.AddListener(StartGame);
-        _soundButton.onClick.RemoveAllListeners();
-        _soundButton.onClick.AddListener(SoundOnOff);
         _infoButton.onClick.RemoveAllListeners();
         _infoButton.onClick.AddListener(() => _panelsController.ActivatedPanel(_panelsController.InfoPanel));
         _closeInfoButton.onClick.RemoveAllListeners();
@@ -41,33 +33,5 @@ public class MenuController : MonoBehaviour
     {
         Time.timeScale = 1f;
         _panelsController.ActivatedPanel(null);
-    }
-
-    void SoundOnOff()
-    {
-        isSoundEnabled = !isSoundEnabled;
-
-        AudioListener.volume = isSoundEnabled ? 1f : 0f;
-
-        PlayerPrefs.SetInt("Sound", isSoundEnabled ? 1 : 0);
-
-        UpdateSoundIcon();
-    }
-
-    void SoundStart()
-    {
-        isSoundEnabled = PlayerPrefs.GetInt("Sound", 1) == 1;
-
-        AudioListener.volume = isSoundEnabled ? 1f : 0f;
-
-        UpdateSoundIcon();
-    }
-
-    void UpdateSoundIcon()
-    {
-        if (_soundIcon != null)
-        {
-            _soundIcon.sprite = isSoundEnabled ? _soundOnSprite : _soundOffSprite;
-        }
     }
 }
